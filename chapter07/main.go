9f1cafc859fa006ad6a7c517cd6374150f05ba1f
Chaitanya Tyagi <chay2199@gmail.com>
2024-04-08 00:39:41 +0530

    Add chapter 07

---
+++ chapter07/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"bufio"
+	"strings"
+)
+
+func main() {
+	for _, fname := range os.Args[1:] {
+		var lc, wc, cc int
+		file, err := os.Open(fname)
+
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			continue 
+		}
+
+		scan := bufio.NewScanner(file)
+		for scan.Scan() {
+			s := scan.Text()
+
+			wc += len(strings.Fields(s))
+			cc += len(s)
+			lc++
+		}
+
+		fmt.Printf("%7d %7d %7d %7s\n", lc, wc, cc, fname)
+	}
+}
